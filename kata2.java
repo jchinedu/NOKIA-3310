037b7d9ec81c4db771547c00359916b6e5c97c52
jchinedu <jchinedu080@gmail.com>
2025-09-09 16:54:59 +0100

    'progress'

--- kata2.java
+++ kata2.java
@@ -3,4 +3,7 @@ public static String reverseWords(final String original) {
 StringBuilder result = new StringBuilder();
 StringBuilder word = new StringBuilder();
 
+    for (int i = 0; i < original.length(); i++) {
+      
+    
     
