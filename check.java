8a519a8444bce7c403728e65077b7015172972f3
jchinedu <jchinedu080@gmail.com>
2025-10-21 23:54:20 +0100

    'done'

--- check.java
+++ check.java
@@ -1,14 +1,6 @@
 public class check {
 public static void main(String[] args) {
 	int i = 10;
- while(i <= 20) {
-	if(i == 10) {
-	System.out.println("i love you");
-	}
-	i++;
-	System.out.print(i);
-	continue;
-	
-	}
+ 
 }
  }
